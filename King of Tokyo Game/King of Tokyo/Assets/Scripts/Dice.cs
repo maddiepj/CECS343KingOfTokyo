240eec0096d9731f9a706b6858223c641ddc8977
maddiepj <maddiepj@gmail.com>
2019-12-01 18:15:53 -0800

    Added DiceManager and Die scripts which allow the player to roll 6 dice; the random number generator in DiceManager.cs needs to be replace to get 6 different dice faces.

---
+++ King of Tokyo Game/King of Tokyo/Assets/Scripts/Dice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+
+    public string value;
+    string[] faces = { "1", "2", "3", "claw", "lightning", "heart" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setValue(string newVal)
+    {
+        value = newVal;
+
+    }
+
+}
---
+++ King of Tokyo Game/King of Tokyo/Assets/Scripts/DiceManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Dice dice;
+    ArrayList savedDice = new ArrayList();
+    ArrayList diceToRoll = new ArrayList();
+    string[] faces = { "1", "2", "3", "claw", "lightning", "heart" };
+
+    void Start()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Instantiate(dice);
+            diceToRoll.Add(dice);
+            Debug.Log("die " + i + " value: " + dice.value);
+        }
+
+        roll();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int randomNumber(int min, int max)
+    {
+        System.Random random = new System.Random();
+        return random.Next(min, max);
+    }
+
+    public void roll()
+    {
+        foreach (Dice die in diceToRoll)
+        {
+            int randomIndex = randomNumber(0, 6);
+            die.setValue(faces[randomIndex]);
+        }
+
+        for (int i = 0; i < diceToRoll.Count; i++)
+        {
+
+            Debug.Log("die " + i + " value: " + dice.value);
+
+        }
+
+    }
+
+
+}
