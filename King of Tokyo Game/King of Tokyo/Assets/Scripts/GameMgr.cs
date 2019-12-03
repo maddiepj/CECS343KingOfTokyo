e67c1261679cb0b4e877d0bbfcf96a4c3240e5ed
maddiepj <maddiepj@gmail.com>
2019-12-03 12:51:06 -0800

    GameMgr script to keep track of turns

---
+++ King of Tokyo Game/King of Tokyo/Assets/Scripts/GameMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    public MainMenuController menuControl;
+    public List<GameObject> players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = menuControl.getPlayerList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
