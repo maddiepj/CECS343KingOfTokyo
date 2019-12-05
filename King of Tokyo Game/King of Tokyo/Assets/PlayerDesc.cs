314f0a7776ada7a9a9dba8b0a8b5b27bb62ad664
matt19271 <matthew.j.berrios@gmail.com>
2019-12-04 23:20:05 -0800

    reduced to one player and added name desciptor

---
+++ King of Tokyo Game/King of Tokyo/Assets/PlayerDesc.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDesc : MonoBehaviour
+{
+    string name = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setName(string s)
+    {
+        name = s;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+}
